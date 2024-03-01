3a48c023fc811f4b5e405c79db20da7136e9fb69
yaram1008 <yaram.raghu1008@gmail.com>
2024-03-01 16:10:54 +0530

    commit -m "v1.1.1"

---
+++ src/com/dl/one/AreaOfRectangle.java
@@ -0,0 +1,26 @@
+package com.dl.one;
+import java.util.Scanner;
+
+/**
+ Create a class Rectangle with attributes length and width. Implement a method to calculate the area of the rectangle.
+ *
+ */
+
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the length and width of the Rectangle: ");
+        float l = in.nextFloat();
+        float w = in.nextFloat();
+        in.close();
+        float result = areaOfRectangle(l, w);
+
+        System.out.println("The area of rectangle is "+ result);
+    }
+
+    public static float areaOfRectangle(float length, float width) {
+        float area = length * width;
+        return area;
+    }
+}
